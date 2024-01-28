4b3878770dd861b918b157f69e0b479b38571820
faruk <faruk2007016@stud.kuet.ac.bd>
2024-01-29 01:41:14 +0600

    first one

---
+++ sketch_jan28a.ino
@@ -0,0 +1,83 @@
+//port variables needed
+int a1=1;
+int a2=2;
+int a=3;
+int b=4;
+int c=5;
+int d=6;
+int e=7;
+int f=8;
+int g=9;
+int h=10;
+int i=11;
+int j=12;
+int k=13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(a1,OUTPUT);
+  pinMode(a2,OUTPUT);
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+  pinMode(h,OUTPUT);
+  pinMode(i,OUTPUT);
+  pinMode(j,OUTPUT);
+
+pinMode(k,OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //delay(2000);
+  digitalWrite(a1,HIGH);
+  delay(1000);
+  digitalWrite(a2,HIGH);
+  delay(1000);
+  digitalWrite(a,HIGH);
+  delay(1000);
+  digitalWrite(b,HIGH);
+  delay(1000);
+  digitalWrite(c,HIGH);
+  delay(1000);
+  digitalWrite(d,HIGH);
+  delay(1000);
+  digitalWrite(e,HIGH);
+  delay(1000);
+  digitalWrite(f,HIGH);
+  delay(1000);
+  digitalWrite(g,HIGH);
+  delay(1000);
+  digitalWrite(h,HIGH);
+  delay(1000);
+  digitalWrite(i,HIGH);
+  delay(1000);
+  digitalWrite(j,HIGH);
+  delay(1000);
+  digitalWrite(k,HIGH);
+  delay(1000);
+  digitalWrite(a1,LOW);
+  digitalWrite(a2,LOW);
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+  digitalWrite(h,LOW);
+  digitalWrite(i,LOW);
+  digitalWrite(j,LOW);
+  digitalWrite(k,LOW);
+  delay(1000);
+
+
+
+}
